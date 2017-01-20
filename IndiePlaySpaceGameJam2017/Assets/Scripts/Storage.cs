489b92d34024afe5c56e20278a5f921bf6c7cb01
Ludovic <ludovicaubut@hotmail.com>
2017-01-19 23:36:03 -0500

    Polished the input map and added a title comment to every script.

---
+++ IndiePlaySpaceGameJam2017/Assets/Scripts/Storage.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+
+[Serializable]
+public class Storage {
+
+	public int iterationNumber = 0;
+	public List<Leader> leaderList = new List<Leader>();
+}
