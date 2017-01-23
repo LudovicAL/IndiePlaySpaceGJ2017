8809b2e99803c5f902983f2999afc981528fe76b
Ludovic <ludovicaubut@hotmail.com>
2017-01-23 18:11:48 -0500

    Removed every Unity Project files so I can resubmit them in the correct folder.

--- IndiePlaySpaceGameJam2017/Assets/Scripts/Storage.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-
-[Serializable]
-public class Storage {
-
-	public int iterationNumber = 0;
-	public List<Leader> leaderList = new List<Leader>();
-}
